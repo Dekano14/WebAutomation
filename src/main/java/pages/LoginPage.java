49c7ae58104652b410ed97a923cb6af57523de4c
Dekano14 <leonbt000@hotmail.com>
2024-11-03 16:24:06 -0600

    update para el LoginPage

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,39 +1,42 @@
 package pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Credentials;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import utils.Helpers;
 
 import java.time.Duration;
 
 public class LoginPage {
 
+    // WebDriver instance to interact with the browser
     private WebDriver driver;
-    private Credentials credentials;
-    private Helpers helpers;
 
+    // Constructor initializes the driver for the current test session
     public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
 
+    // Locators for the username field, password field, and login button
     private By userField = By.name("username");
     private By passwordField = By.name("password");
     private By loginButton = By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button");
 
+    //Performs login by waiting for elements to be visible, then entering credentials and clicking the login button
     public void login(String user, String password) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Waits for the username field and inputs the username
         WebElement userElement = wait.until(ExpectedConditions.presenceOfElementLocated(userField));
         userElement.sendKeys(user);
-        //
+
+        // Waits for the password field and inputs the password
         WebElement passElement = wait.until(ExpectedConditions.presenceOfElementLocated(passwordField));
         passElement.sendKeys(password);
 
+        // Waits for the login button to be clickable and clicks to submit the form
         WebElement loginBtn = wait.until(ExpectedConditions.elementToBeClickable(loginButton));
         loginBtn.click();
-
     }
 }
